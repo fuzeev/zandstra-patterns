90011fbc4ad6d198f886154a794f427cd0685bd7
Олег Фузеев <oleg.fuzeev02@gmail.com>
2023-09-10 18:13:36 +0300

    реализация синглтона

--- src/Singleton/Singleton.php
+++ src/Singleton/Singleton.php
@@ -4,8 +4,30 @@ namespace Youngsayora\Zandstra\Singleton;
 
 class Singleton
 {
-    public function getInstance()
+    private array $props = [];
+    private static Singleton $instance;
+
+    private function __construct()
+    {      
+    }
+    
+    public static function getInstance(): Singleton
     {
-        dd('autoload работает');
+        if(empty(self::$instance))
+        {
+            self::$instance = new Singleton;
+        }
+
+        return self::$instance;
+    }
+
+    public function setProperty(string $key, string $val): void 
+    {
+        $this->props[$key] = $val;
+    }
+
+    public function getProperty(string $key): string 
+    {
+        return $this->props[$key];
     }
 }
--- web/index.php
+++ web/index.php
@@ -4,6 +4,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Youngsayora\Zandstra\Singleton\Singleton;
 
-$singleton = new Singleton;
+// //Singletone
+// $singleton = Singleton::getInstance();
+// $singleton->setProperty('228', 'bruh');
 
-$singleton->getInstance();
+// $singleton2 = Singleton::getInstance();
+// dd($singleton2->getProperty('228'), $singleton, $singleton2);
